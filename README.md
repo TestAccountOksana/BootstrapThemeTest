a833b66cee4d74bab08b7fca73cb4b02515490bf
Your Name <you@example.com>
2017-05-14 01:08:17 +0300

    added boottrap

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bootstrapTheme
+
+Project setup:
+1) crete folder
+2) clone project
+3) npm up
+
+To setup any other plugins and adding them to dependecies:
+
+npm install --save-dev gulp-autoprefixer
--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@
  * Created by oksana on 13.05.17.
  */
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    livereload = require('gulp-livereload');
 
 gulp.task('scss', function() {
     gulp.src('./resourses/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('./resourses/css/'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
         .pipe(gulp.dest('./resourses/css/'));
 });
 
