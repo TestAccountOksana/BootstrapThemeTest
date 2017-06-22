31f9e7a2685ac1dcac5a438ede55c1f59ae78c37
azarova <azarovaoksana15@gmail.com>
2017-06-22 14:25:21 +0300

    added and configurated browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,32 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
-    livereload = require('gulp-livereload');
+    browserSync = require('browser-sync').create();
+
+gulp.task('serve', ['scss'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./resourses/scss/*.scss", ['scss']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
 
 gulp.task('scss', function() {
     gulp.src('./resourses/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(autoprefixer(
+            'last 2 version',
+            'safari 5',
+            'ie 8', 'ie 9',
+            'opera 12.1',
+            'ios 6',
+            'android 4'))
         .pipe(gulp.dest('./resourses/css/'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(minifycss())
-        .pipe(gulp.dest('./resourses/css/'));
+        .pipe(gulp.dest('./resourses/css/'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('scss:watch', function() {
-    gulp.watch('./resourses/scss/*.scss', ['scss']);
-})
